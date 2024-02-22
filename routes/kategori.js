c9069042abeefa2fe80a83540b49c5db5174a9ba
thoriqbani <tjrsgamers@gmail.com>
2024-02-22 13:07:15 +0700

    2

--- routes/kategori.js
+++ routes/kategori.js
@@ -15,4 +15,74 @@ router.get('/', function(req, res, next){
     })
 })
 
+router.get('/create', function(req,res){
+    res.render('kategori/create', {
+        nama: ''
+    })
+})
+
+router.get('/edit/(:id)', function(req, res){
+    const id = req.params.id;
+    connect.query('select * from kategori where id = ?', id, function(err, rows){
+        if(err){
+            req.flash('error','Query gagal')
+        } else {
+            res.render('kategori/edit',{
+                id: rows[0].id,
+                nama: rows[0].nama
+            })
+        }
+    })
+})
+
+router.post('/store', function(req, res){
+    try {
+        const nama = req.body
+        connect.query('insert into kategori set ? ', nama, function(err, result){
+            if(err){
+                req.flash('error','Gagal menyimpan data')
+            } else {
+                req.flash('success','Berhasil menyimpan data')
+            }
+            res.redirect('/kategori')
+        })
+    } catch(error) {
+        console.error(error)
+        req.flash('error', 'Terjadi Kesalahan')
+        res.redirect('/kategori')
+        
+    }
+})
+
+router.post('/update/(:id)', function(req, res){
+    try{
+        const id = req.params.id;
+        const Data = {
+            nama: req.body.nama
+        }
+        connect.query('update kategori set ? where id = '+ id, Data, function(err){
+            if(err){
+                req.flash('error','gagal memperbaharui data')
+            } else {
+                res.redirect('/kategori')
+            }
+        })
+    } catch(error) {
+        req.flash('error','terjadi kesalahan pada fungsi')
+        res.render('/kategori')
+    }
+})
+
+router.get('/delete/(:id)', function(req, res) {
+    const id = req.params.id
+    connect.query('delete from kategori where id = ?', id, function(err){
+        if(err){
+            req.flash('error','Gagal menghapus')
+        } else {
+            req.flash('success','Data terhapus')
+        }
+        res.redirect('/kategori')
+    })
+})
+
 module.exports = router
